096de5f64662c0164d6c5d249f61b1087ecfe1b9
Michael Mount <mount.photos@gmail.com>
2023-02-12 10:38:06 -0700

    Leftover Note left in comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,93 +1,14 @@
 // VARIBILE DECLERATION
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var specialCharater = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  ",",
-  "(",
-  ")",
-  "*",
-  "+",
-  "'",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "]",
-  "^",
-  "_",
-  "'",
-  "{",
-  "}",
-  "|",
-  "~",
-];
+
+var lowerCase = ["a","b","c","d", "e","f","g","h","i","j",
+  "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L",
+  "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+
+  var specialCharater = ["!","#","$","%","&",",","(",")","*","+","'",
+  "-",".","/",":",";","<","=",">","?","@","[","]","^","_","'","{","}","|","~",];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -165,4 +86,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// var password = generatePassword();
